1a2277e5239188a9acea8d776599706966cdd9db
Amnon Bleich <amnon@timeslot.com>
2017-07-01 22:29:45 +0200

    ADD: c3-c9 feedback+async analysis

--- Final/models/apoptosis.R
+++ Final/models/apoptosis.R
@@ -1,21 +1,31 @@
 library(BoolNet)
-setwd("~/System_Biology/Final/models/mathias_v1")
+# setwd("~/System_Biology/Final/models/")
+setwd("~/Workspace/uni/System_Biology/Final/models/")
 
 extrinsic <- loadNetwork("extrinsic.boolnet")
 intrinsic <- loadNetwork("intrinsic.boolnet")
+intrinsic_feedback <- loadNetwork("intrinsic_feedback.boolnet")
 
 plotNetworkWiring(extrinsic)
 plotNetworkWiring(intrinsic)
+plotNetworkWiring(intrinsic_feedback)
 
 start_state_ex = c(1,0,0,0,0)
 # start_state_in = c(1,1,0,0,1,1,1,0,0,0,0,0)
 start_state_in = c(1,0,0,1,0,0,0,0,0,0,0)
+start_state_in_feedback = c(1,0,0,1,0,0,0,0,0,0,0,0)
 
 plotSequence(extrinsic, startState = start_state_ex)
 plotSequence(intrinsic, startState = start_state_in)
+plotSequence(intrinsic_feedback, startState = start_state_in_feedback)
 
 attractors_in=getAttractors(intrinsic, type="synchronous", method="exhaustive", returnTable=T)
 plotAttractors(attractors_in, drawLegend=F)
 
 attractors_in_chosen=getAttractors(intrinsic, type="synchronous", method='chosen', startStates = list(start_state_in), returnTable=T)
 plotAttractors(attractors_in_chosen)
+
+attractors_in_feedback=getAttractors(intrinsic_feedback, type="asynchronous", method="chosen", startStates = list(start_state_in_feedback),  returnTable=T)
+attractors_in_feedback=getAttractors(intrinsic_feedback, type="synchronous", method="chosen", startStates = list(start_state_in_feedback),  returnTable=T)
+plotAttractors(attractors_in_feedback, drawLegend=T)
+#### short remark: when IAP is present, there is no apoptosis. without is, bot apoptosis and no apoptosis are possible. in a synchronous network, there's always apoptosis
